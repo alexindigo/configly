b2ccabaece26133310c8fb3266d25ecf7c238345
Alex Indigo <iam@alexindigo.com>
2016-02-20 11:32:55 -0800

    Filling in tests.

---
+++ test/test-basic.js
@@ -0,0 +1,19 @@
+var testName = 'basic';
+var tap      = require('tap');
+var path     = require('path');
+var config   = require('../');
+var expected = require('./fixtures/expected/' + testName + '.json');
+var configObj;
+
+// augment `process.env` for stable testing`
+process.env['NODE_ENV']    = 'staging';
+process.env['EMPTY_VAR']   = '';
+process.env['TESTLINEONE'] = 'ABC';
+process.env['TESTLINETWO'] = 'XYZ';
+process.env['JUST_VAR']    = 'A VAR';
+process.env['VARPART1']    = 'COMBINED VAR 1/2';
+process.env['VARPART2']    = 'COMBINED VAR 2/2';
+
+configObj = config(path.join(__dirname, 'fixtures/config/' + testName));
+
+tap.same(configObj, expected);
--- test/test-broken_env_vars.js
+++ test/test-broken_env_vars.js
@@ -1,9 +1,10 @@
+var testName = 'broken_env_vars';
 var tap      = require('tap');
 var path     = require('path');
 var config   = require('../');
-var expected = require('./fixtures/expected/initial.json');
 
 // augment `process.env` for stable testing`
+process.env['NODE_ENV']    = 'staging';
 process.env['EMPTY_VAR']   = '';
 process.env['TESTLINEONE'] = 'ABC';
 process.env['TESTLINETWO'] = 'XYZ';
@@ -11,6 +12,9 @@ process.env['JUST_VAR']    = 'A VAR';
 process.env['VARPART1']    = 'COMBINED VAR 1/2';
 process.env['VARPART2']    = 'COMBINED VAR 2/2';
 
-var configObj = config(path.join(__dirname, 'fixtures/config/initial'));
-
-tap.same(configObj, expected);
+tap.throws(function()
+{
+  config(path.join(__dirname, 'fixtures/config/' + testName));
+},
+{message: 'Illegal key type for custom-environment-variables at Arrays Not Supported: [object Array]'},
+'expected to throw on array element within custom-environment-variables file');
