bd4a9aa84914ec6695d7e8ef609e8abec9d56e78
Alex Indigo <iam@alexindigo.com>
2016-07-29 17:26:54 -0700

    #1 Fixed env variables issue (value === name)

---
+++ test/test-env_vars_same.js
@@ -0,0 +1,21 @@
+var testName  = 'env_vars_same';
+var test      = require('tape');
+var path      = require('path');
+var config    = require('../');
+var expected  = require('./fixtures/expected/' + testName + '.json');
+var configDir = path.join(__dirname, 'fixtures/config/' + testName);
+
+test('broken custom environment variables', function(t)
+{
+  t.plan(1);
+
+  var configObj;
+
+  // augment `process.env` for stable testing`
+  process.env['VALUE'] = 'VALUE';
+  process.env['VAL3']  = 'three';
+  process.env['VAL4']  = 'four';
+
+  configObj = config(configDir);
+  t.deepLooseEqual(configObj, expected, 'expects to get right config object');
+});
