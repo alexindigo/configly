afc771d5388e405f79eb8918411f796f4fd889f1
Alex Indigo <iam@alexindigo.com>
2018-08-11 16:26:07 -0700

    Added malformed date test

---
+++ test/test-env_vars_modifiers_date_malformed.js
@@ -0,0 +1,20 @@
+var testName  = 'env_vars_modifiers_date_malformed';
+var test      = require('tape');
+var path      = require('path');
+var config    = require('../');
+var configDir = path.join(__dirname, 'fixtures/config/' + testName);
+
+test('malformed date environment variable with date modifier', function(t)
+{
+  t.plan(1);
+
+  // augment `process.env` for stable testing`
+  process.env['SOMETIME_NEVER'] = 'If it doesn\'t look like date';
+
+  t.throws(function()
+  {
+    config({ directories: configDir });
+  },
+  {message: 'Unable to parse provided value `If it doesn\'t look like date` with `date` modifier. Exception: Invalid Date'},
+  'expects to throw on malformed date within date modifier of custom-environment-variables file');
+});
