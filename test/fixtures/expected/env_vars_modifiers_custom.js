5d53e288597c4a2e8a5b7f1b6b6b9147bda5f42d
Alex Indigo <iam@alexindigo.com>
2018-11-26 22:18:19 -0800

    Workaround for weird Travis-ci behavior

--- test/fixtures/expected/env_vars_modifiers_custom.js
+++ test/fixtures/expected/env_vars_modifiers_custom.js
@@ -1,4 +1,5 @@
 var moment = require('moment');
+var deeply = require('deeply');
 
 var datetime = moment.utc('2016-01-01T00:00:00');
 var date = new Date('Fri Jan 01 2016 00:00:00 GMT-0800');
@@ -6,7 +7,7 @@ var combinedDate = new Date('Thu Dec 31 2015 16:00:00 GMT-0800');
 
 module.exports = {
   'stuff': {
-    'datetime': datetime,
+    'datetime': deeply(datetime),
     'datetime_as_string': datetime.toString(),
 
     'date': date,
